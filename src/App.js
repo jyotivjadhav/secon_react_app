ced25c88f04690d34f13c30b5899ea8cb7d4acb8
Jyoti Jadhav <jyotivjadhav748@gmail.com>
2023-06-13 16:35:41 +0530

    inline styling.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-
+// HTML + JS => JSX
+// Putting the Brain in HTML.
+// Styling :: Color
+// CSS is also JavaScript!!!
 function App() {
-  let str = `Hello Universe!!!`;
-  let id = "id1";
-  let user = { id: 1, name: "rohit" };
+    // object literals
+  let style = { background: "pink", color: "white" };
 
   return (
     <>
-      <h1 id="id1">{ str}</h1>
-      <h1>
-        ID: {user.id} Name: {user.name}
+      <h1 id={"id1"} style={style}>Hello World</h1>
+        {/** First Curly Brace represents JS Expression */}
+      {/** Second Culry Brace, its object literal */}
+      <h1 id={{}}>Hello World</h1>
+      <h1 style={{ background: "blue", color: "white" }}>Hello</h1>
+           {/** Observations */}
+      {/** Style Attributes become CamelCase in Nature */}
+      {/** Values we are wriing in double quotes */}
+      {/** Multiple props are seperate by comma */}
+      <h1 style={{ backgroundColor: "red", color: "white", padding: "8px" }}>
+        hello World
       </h1>
-      <h1 id={ id}>{ str}</h1>
     </>);
 }
 
