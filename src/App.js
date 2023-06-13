056ce55241083f020d7b9468de6572dcc6648f08
Jyoti Jadhav <jyotivjadhav748@gmail.com>
2023-06-13 16:23:11 +0530

    interpolation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 // PURE HTML :: DUMB
 // JSX :: RETURN HTML :: THIS IS SMART
 function App() {
+  let str = `Hello Universe!!!`;
+  let pi = 3.141159;
+  let active = true;
+  let isUserdLoggedIn = true;
+
   return (
     <>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, minima
-        earum exercitationem distinctio consectetur sit illo fugiat rem quaerat
-        cupiditate dicta quidem debitis iste aut maiores? Amet corrupti debitis
-        suscipit.
-      </h1>
-    </>
-  );
+      <h1>Hello World</h1>
+      <h1>{str}</h1>
+      <h1>PI {pi} !!</h1>
+
+      {/**IF Logged In */}
+      {isUserdLoggedIn && <h1>Welcome CDAC!</h1>}
+
+      {/** Usage of Boolean */}
+      <h1>{active ? "good morning" : "good night"}</h1>
+
+      {/** MOST USEFUL  */}
+      {active && <h1>Good Morning</h1>}
+      {active ? <h1>Good Morning</h1> : <h1>Good Night</h1>}
+    </>);
 }
 
 export default App;
