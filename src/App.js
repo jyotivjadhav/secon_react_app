b58402c80b11e6da15ce752f085212df1506bd05
Jyoti Jadhav <jyotivjadhav748@gmail.com>
2023-06-13 16:25:47 +0530

    object interpolation

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@
 // JSX :: RETURN HTML :: THIS IS SMART
 function App() {
   let str = `Hello Universe!!!`;
-  let pi = 3.141159;
-  let active = true;
-  let isUserdLoggedIn = true;
+
+  let user = { id: 1, name: "rohit" };
 
   return (
     <>
-      <h1>Hello World</h1>
-      <h1>{str}</h1>
-      <h1>PI {pi} !!</h1>
-
-      {/**IF Logged In */}
-      {isUserdLoggedIn && <h1>Welcome CDAC!</h1>}
-
-      {/** Usage of Boolean */}
-      <h1>{active ? "good morning" : "good night"}</h1>
-
-      {/** MOST USEFUL  */}
-      {active && <h1>Good Morning</h1>}
-      {active ? <h1>Good Morning</h1> : <h1>Good Night</h1>}
+      <h1>{ str}</h1>
+      <h1>
+        ID: {user.id} Name: {user.name}
+      </h1>
     </>);
 }
 
