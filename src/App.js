da6d949569af447f993a55e66032b4837afad243
Jyoti Jadhav <jyotivjadhav748@gmail.com>
2023-06-13 16:28:25 +0530

    attribute binding.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-// PURE HTML :: DUMB
-// JSX :: RETURN HTML :: THIS IS SMART
+
 function App() {
   let str = `Hello Universe!!!`;
-
+  let id = "id1";
   let user = { id: 1, name: "rohit" };
 
   return (
     <>
-      <h1>{ str}</h1>
+      <h1 id="id1">{ str}</h1>
       <h1>
         ID: {user.id} Name: {user.name}
       </h1>
+      <h1 id={ id}>{ str}</h1>
     </>);
 }
 
